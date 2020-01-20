44aaea170a8071da2e9b8e030fb3a126fbd47c3f
pwjworks <478624590@qq.com>
2020-01-21 01:28:56 +0800

    add header,main panel.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world')
+import Vue from 'vue'
+import App from './App.vue'
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+/* eslint-disable no-new */
+new Vue({
+  render: h => h(App)
+}).$mount(root)
