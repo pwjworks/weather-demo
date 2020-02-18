e185ab3aa276dd58ae037eb0922b38b3b4a4c983
pwjworks <13610513466@163.com>
2020-02-19 00:23:26 +0800

    api

--- server/routers/api.js
+++ server/routers/api.js
@@ -2,7 +2,7 @@
  * @Author: pwjworks
  * @Date: 2020-02-15 00:43:22
  * @Last Modified by: pwjworks
- * @Last Modified time: 2020-02-19 00:15:29
+ * @Last Modified time: 2020-02-19 00:23:09
  */
 // const axios = require('axios')
 const Router = require('koa-router')
@@ -12,13 +12,12 @@ const apiRouter = new Router({
 })
 
 apiRouter.get('/IPLocation', async (ctx) => {
-  const keys = Object.keys(ctx.req.headers)
-  console.log('req:' + keys)
-  console.log('xrealip:' + ctx.req.headers['x-real-ip'])
-  console.log('x-forwarded-for:' + ctx.req.headers['x-forwarded-for'])
-  console.log('host:' + ctx.req.headers.host)
+  const index = ctx.req.headers['x-forwarded-for'].indexOf(',')
+  const clientIP = ctx.req.headers['x-forwarded-for'].substr(0, index === -1 ? ctx.req.headers['x-forwarded-for'].length : index)
+  console.log(clientIP)
   const resp = await ctx.handler.getWeatherAPIResp({
     app_key: 'F3A4E792DCFE446D889CB97357310B1B',
+    ip: clientIP,
     s: 'App.Open_Baidu.LocationIP'
   })
   if (ctx.city === '' && resp.data.content.address_detail.city !== '') {
